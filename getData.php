6dfe31e3a51d54476132ca049af06cb3b4ed632a
Bill Lugo <11304397+Billman64@users.noreply.github.com>
2023-11-05 02:17:15 -0500

    Google sheet API data pull implemented!

--- getData.php
+++ getData.php
@@ -1,26 +1,31 @@
 <?php
 
-require("auth.php");
+
 
 // url (user perspective):
-//"https://docs.google.com/spreadsheets/d/16nMu6cKQNhVMr4sMFxsW_BblpqCfSAMYXMo-GtYRxMw/edit#gid=0";
+//https://docs.google.com/spreadsheets/d/16nMu6cKQNhVMr4sMFxsW_BblpqCfSAMYXMo-GtYRxMw/edit#gid=0 Hawaii
+//https://docs.google.com/spreadsheets/d/18OtJmXAK4xGQD-0OOjUf9vAX7uETiIemoo6okfQ0YZY/edit#gid=0 NJ (layoffdata.com)
+//https://docs.google.com/spreadsheets/d/1s2FUd66fdhvKpC_tcS_CkSjYywixLl3Fa04OU8gCekQ/edit#gid=0 all states
 
 // API URL: "https://sheets.googleapis.com/v4/spreadsheets/[SPREADSHEETID]?key=[YOUR_API_KEY]";
 
-$sheetId = "16nMu6cKQNhVMr4sMFxsW_BblpqCfSAMYXMo-GtYRxMw";
-$ApiKey = "";
-$url = "https://sheets.googleapis.com/v4/spreadsheets/[" . $sheetId . "]?key=[" . $ApiKey . "]";
-$accessToken = "";
+//$sheetId = "16nMu6cKQNhVMr4sMFxsW_BblpqCfSAMYXMo-GtYRxMw"; // Hawaii
+$sheetId = "1s2FUd66fdhvKpC_tcS_CkSjYywixLl3Fa04OU8gCekQ"; // all states (layoffdata.com)
+require("auth.php");
+$range = "a1:m100000";
+$url = "https://sheets.googleapis.com/v4/spreadsheets/" . $sheetId .  "/values/" . $range . "?access_token=" . $accessToken . "&key=" . $ApiKey; 
 
-
+/*$headers = array(
+	"--header 'Authorization: Bearer " . $accessToken . "'",
+	"--header 'Accept: application/json'",
+	"compressed"
+	); */
+	
 $headers = array(
-	"--header Authorization: Bearer [" . $accessToken . "]",
-	"--header Accept: application/json"
+	"'Authorization: Bearer " . $accessToken . "'",
+	"'Accept: application/json'",
+	"compressed"
 	);
-
-$header1 = "--header Authorization: Bearer [" . $accessToken . "]";
-$header2 = "--header Accept: application/json";
-$header3 = "compressed";
 
 
 // Grab Google Sheet for data  (monthly cron job ideally)
@@ -34,20 +39,22 @@ curl_setopt($curl, CURLOPT_URL, "$url");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-$response = curl_exec($curl);
 
+$response = curl_exec($curl);
 
 if($response == false){
 	echo "cURL error: #" .  curl_errno($curl) . " " . curl_error($curl);
 } else {
-	curl_close($curl);
+	//$curlInfo = curl_getinfo($curl);
 	echo $response;
 }
+	file_put_contents("data/allStates.json", $response);
+	curl_close($curl);
+
+// Save to a file for many lookups
 
 
-
-
-// Get data (arrayList of some layoffNotice object?)
+// Get data (array of some layoffNotice object?)
 // json_decode(response)
 
 
