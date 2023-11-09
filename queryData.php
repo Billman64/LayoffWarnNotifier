1418b5394e59506b76106306cb10e247260bb2a1
Bill Lugo <11304397+Billman64@users.noreply.github.com>
2023-11-08 23:09:29 -0500

    State selector drop-down
    State selection in the UI now implemented via GET postback, but only works for 3 states so far.

--- queryData.php
+++ queryData.php
@@ -2,14 +2,18 @@
 
 $filename = "allStates.json";
 $location = "data/";
-if(isset($_GET["state"])) {$state = statenameTransform($_GET["state"]);}
-else $state = "";	// TODO: shortcut if then else (ternary statement)  $condition ? $value_if_true : $value_if_false
-
+if(isset($_GET["state"])) {$state = statenameTransform($_GET["state"]);
+	echo "WARN layoff notice data for " . statenameTransform($state) . "<br>";
+} else {
+	$state = "";	// TODO: shortcut if then else (ternary statement)  $condition ? $value_if_true : $value_if_false
+	echo "WARN layoff notice data. Select a state.";
+	selectStateForm();
+}
 
 // testing area
 $date = getdate();
 
-echo "state = " . $state . "<br>";
+
 // echo "date = " . $date["month"] . "<br>"; echo "date = " . getdate()["year"] . "<br>";
 //$a = $date["mon"]."/". $date["mday"]."/". $date["year"];
 //echo $a;
@@ -56,10 +60,13 @@ if($data!=""){
 	
 	
 	
-	
+		
 }
 
-
+if($state != "") {
+	echo "<br>";
+	selectStateForm();
+}
 
 // check if given date is this month or later
 function isFutureDate($inputDate){		
@@ -90,7 +97,8 @@ function isFutureDate($inputDate){
 function statenameTransform($state) {
 	$longform = "";
 	
-	switch(strtolower($state)){		// TODO: consider a hashmap, hashset, or other key-value pair collection
+	switch(strtolower($state)){		// TODO: consider a hashmap
+		case "al": $longform = "Alabama"; break;
 		case "nj": $longform = "New Jersey"; break;
 		case "oh": $longform = "Ohio"; break;
 		default: $longform = $state;
@@ -99,6 +107,67 @@ function statenameTransform($state) {
 }
 
 
-
+function selectStateForm($color="#3df"){
+	
+	echo '<div><span style="background: '. $color .'"><form style="margin:10px;">Select state:<br>';
+	
+	// TODO: refactor, possibly using a shared hashmap in a loop
+	echo '<select name="state">
+	<option value="AL">Alabama</option>
+	<option value="AK">Alaska</option>
+	<option value="AZ">Arizona</option>
+	<option value="AR">Arkansas</option>
+	<option value="CA">California</option>
+	<option value="CO">Colorado</option>
+	<option value="CT">Connecticut</option>
+	<option value="DE">Delaware</option>
+	<option value="DC">District Of Columbia</option>
+	<option value="FL">Florida</option>
+	<option value="GA">Georgia</option>
+	<option value="HI">Hawaii</option>
+	<option value="ID">Idaho</option>
+	<option value="IL">Illinois</option>
+	<option value="IN">Indiana</option>
+	<option value="IA">Iowa</option>
+	<option value="KS">Kansas</option>
+	<option value="KY">Kentucky</option>
+	<option value="LA">Louisiana</option>
+	<option value="ME">Maine</option>
+	<option value="MD">Maryland</option>
+	<option value="MA">Massachusetts</option>
+	<option value="MI">Michigan</option>
+	<option value="MN">Minnesota</option>
+	<option value="MS">Mississippi</option>
+	<option value="MO">Missouri</option>
+	<option value="MT">Montana</option>
+	<option value="NE">Nebraska</option>
+	<option value="NV">Nevada</option>
+	<option value="NH">New Hampshire</option>
+	<option value="NJ">New Jersey</option>
+	<option value="NM">New Mexico</option>
+	<option value="NY">New York</option>
+	<option value="NC">North Carolina</option>
+	<option value="ND">North Dakota</option>
+	<option value="OH">Ohio</option>
+	<option value="OK">Oklahoma</option>
+	<option value="OR">Oregon</option>
+	<option value="PA">Pennsylvania</option>
+	<option value="RI">Rhode Island</option>
+	<option value="SC">South Carolina</option>
+	<option value="SD">South Dakota</option>
+	<option value="TN">Tennessee</option>
+	<option value="TX">Texas</option>
+	<option value="UT">Utah</option>
+	<option value="VT">Vermont</option>
+	<option value="VA">Virginia</option>
+	<option value="WA">Washington</option>
+	<option value="WV">West Virginia</option>
+	<option value="WI">Wisconsin</option>
+	<option value="WY">Wyoming</option>
+</select> ';
+echo '<button type="submit" style="height: 30px; width: 70;">submit</button>';
+echo '</form></span></div>';
+	
+}
 
 ?>
